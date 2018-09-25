0a9db214670b814927587ae522a96d3cee0687fc
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-25 02:33:05 +0200

    feat(project): add DbRoute class for creating crud routes

---
+++ src/Library/DbRoute.ts
@@ -0,0 +1,33 @@
+import { Route } from 'stix';
+
+export class DbRoute {
+  public static find (endpoint: string, controller: string | { new (): Object }) {
+    return Route.get(endpoint, controller, 'find');
+  }
+
+  public static findOne (endpoint: string, controller: string | { new (): Object }) {
+    return Route.get(`${endpoint}/:id`, controller, 'find');
+  }
+
+  public static create (endpoint: string, controller: string | { new (): Object }) {
+    return Route.post(endpoint, controller, 'create');
+  }
+
+  public static modify (endpoint: string, controller: string | { new (): Object }) {
+    return Route.patch(`${endpoint}/:id`, controller, 'modify');
+  }
+
+  public static destroy (endpoint: string, controller: string | { new (): Object }) {
+    return Route.delete(`${endpoint}/:id`, controller, 'destroy');
+  }
+
+  public static all (endpoint: string, controller: string | { new (): Object }) {
+    return [
+      this.find(endpoint, controller),
+      this.findOne(endpoint, controller),
+      this.create(endpoint, controller),
+      this.modify(endpoint, controller),
+      this.destroy(endpoint, controller),
+    ];
+  }
+}
